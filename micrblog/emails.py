3d6297c72a82e80b33ba9c42e2b513a53e399eb2
Bless_L <664007627@qq.com>
2015-11-13 19:36:35 +0800

    micrblog

---
+++ micrblog/emails.py
@@ -0,0 +1,34 @@
+from flask.ext.mail import Message
+from app import mail
+
+def send_email(subject, sender, recipients, text_body, html_body):
+    msg = Message(subject, sender=sender, recipients=recipients)
+    msg.body = text_body
+    msg.html = html_body
+    mail.send(msg)
+
+from flask import render_template
+from config import ADMINS
+
+def follower_notification(followed, follower):
+    send_email("[microblog] %s is now following you!" % follower.nickname,
+               ADMINS[0],
+               [followed.email],
+               render_template("follower_email.txt", 
+                               user=followed, follower=follower),
+               render_template("follower_email.html", 
+                               user=followed, follower=follower))
+
+from app import app
+from .decorators import async
+
+@async
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+def send_email(subject, sender, recipients, text_body, html_body):
+    msg = Message(subject, sender=sender, recipients=recipients)
+    msg.body = text_body
+    msg.html = html_body
+    send_async_email(app, msg)
